9818f4716564647555f836264403c107e1e861dc
swathikrishnaus <swathikrishnaus2015@gmail.com>
2023-01-24 19:04:32 +0530

    added

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
 $(document).scroll(function () {
     var scroll = $(this).scrollTop();
-    if (scroll > 50)
-        $(".navbar").addClass("nav-shadow")
-    else
-        $(".navbar").removeClass("nav-shadow")
+    if (scroll > 50) {
+        $(".nav-bar").addClass("nav-shadow");
+        $(".nav-bar .logo").removeClass("logo-fixed");
+    }
+    else {
+        $(".nav-bar").removeClass("nav-shadow");
+        $(".nav-bar .logo").addClass("logo-fixed");
+    }
 })
